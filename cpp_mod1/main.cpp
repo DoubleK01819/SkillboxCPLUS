ae87e693462fc01e1e5cd61ce3a47d1b04bb4cea
Kiryxa <66702774+DoubleK01819@users.noreply.github.com>
2024-05-12 16:37:00 +0300

    Add files via upload

---
+++ cpp_mod1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double f1 = 0.0;
+    double f2 = 0.0;
+    double f3 = 0.0;
+    double f4 = 0.0;
+    double f5 = 0.0;
+
+    cout<<"Fibonacchi number 1 : "<< f1 << endl;
+    cout<<"Fibonacchi number 2 : "<< f2 << endl;
+    cout<<"Fibonacchi number 3 : "<< f3 << endl;
+    cout<<"Fibonacchi number 4 : "<< f4 << endl;
+    cout<<"Fibonacchi number 5 : "<< f5 << endl;
+}
